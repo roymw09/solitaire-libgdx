7d0a696a5df414eecf6037138faac48492e79197
roymw09 <60860251+roymw09@users.noreply.github.com>
2022-01-26 16:23:10 -0330

    Initial commit

---
+++ desktop/src/com/solitaire/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.solitaire.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.solitaire.game.SolitaireGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SolitaireGame(), config);
+	}
+}
