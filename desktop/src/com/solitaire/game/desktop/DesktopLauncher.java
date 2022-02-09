b6ef4f178308e9fa9832b47c2ab23f83c081627c
Michael <michaelthomas0721@icloud.com>
2022-02-09 13:20:14 -0500

    Fixed illegal reflective access operation has occured error

--- desktop/src/com/solitaire/game/desktop/DesktopLauncher.java
+++ desktop/src/com/solitaire/game/desktop/DesktopLauncher.java
@@ -2,14 +2,15 @@ package com.solitaire.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.solitaire.game.SolitaireGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Solitaire";
-		config.width = 800;
-		config.height = 480;
-		new LwjglApplication(new SolitaireGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Solitaire");
+		config.setWindowedMode(800, 480);
+		new Lwjgl3Application(new SolitaireGame(), config);
 	}
 }
